478a9943bd33b2b6b9938925d168e2370ebf8490
Michael Tu <tuzhucheng@gmail.com>
2013-07-11 19:44:20 -0400

    web.js read from file

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buffer = fs.readFileSync("index.html");
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
