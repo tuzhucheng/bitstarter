8e75dda16c33ff82363e41f0306033ed8c94a9af
Michael Tu <tuzhucheng@gmail.com>
2013-07-19 00:33:50 -0400

    grader.js from hw3 and styled, responsive index.html with Bootstrap

--- grader.js
+++ grader.js
@@ -24,8 +24,12 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://aqueous-scrubland-4028.herokuapp.com";
+
+var checkJson;
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -34,6 +38,15 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var assertURLExists = function(url) {
+    rest.get(url).on('complete', function(result) {
+	if (result instanceof Error)
+	    process.exit(1);
+	else
+	    return result;
+    });
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -61,14 +74,25 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var processing = function() {
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to remote index.html', clone(assertURLExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+	//there is url flag
+	checkJson = checkHtmlFile(program.url, program.checks);
+    } else {
+        //normal processing
+        checkJson = checkHtmlFile(program.file, program.checks);
+    }
+    processing();
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
